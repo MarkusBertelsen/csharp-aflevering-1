1c61b7eb10f7d02e62daeafc4060abd35e6cde27
Markus Bertelsen <markusb-97@hotmail.com>
2020-03-06 11:47:20 +0100

    Initial commit

---
+++ 1. Aflevering/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Aflevering
+{
+    class User
+    {
+        public int id { get; }
+        public string name { get; }
+        public int age { get; }
+        public int score { get; }
+
+        public User(int id, string name, int age, int score)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.score = score;
+        }
+
+        public override string ToString()
+        {
+            return name + ", id: " + id;
+        }
+    }
+}
---
+++ 1. Aflevering/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1.Aflevering
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string text;
+                var streamReader = new StreamReader(openFileDialog.FileName);
+                while((text = streamReader.ReadLine()) != null)
+                {
+                    string[] values = text.Split(';');
+                    User user = new User(int.Parse(values[0]), values[1], int.Parse(values[2]), int.Parse(values[3]));
+                    listBox.Items.Add(user);
+                }
+                listCounter.Content = "Amount of users in list: " + listBox.Items.Count.ToString();
+                lastLoadTimer.Content = "Last time users loaded: " + DateTime.Now.ToString();
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            User u = listBox.SelectedItem as User;
+            idTextBox.DataContext = u;
+            nameTextBox.DataContext = u;
+            ageTextBox.DataContext = u;
+            scoreTextBox.DataContext = u;
+        }
+    }
+}
